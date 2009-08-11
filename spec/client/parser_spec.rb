8e3cd400a38509432dadb597b1001b57d26d69c1
brainopia <ravwar@gmail.com>
2009-08-12 02:52:48 +0400

    Add couple of specs to parser

--- spec/client/parser_spec.rb
+++ spec/client/parser_spec.rb
@@ -1,18 +1,22 @@
 require 'lib/client/parser'
 
 describe Client::Parser do
-  it 'should parse sexps with one item' do
+  it 'should parse sexps with integer' do
     Client::Parser.run('(a 5)').should == { :a => 5 }
   end
 
+  it 'should parse sexps with float' do
+    Client::Parser.run('(c 3.3)').should == { :c => 3.3 }
+  end
+
+  it 'should parse sexps with text' do
+    Client::Parser.run('(c text)').should == { :c => :text }
+  end
+  
   it 'should parse sexps with multiple items' do
     Client::Parser.run('(b 1 2)').should == { :b => [1, 2] }
   end
-
-  it 'should parse sexps and convert text to symbols' do
-    Client::Parser.run('(c text)').should == { :c => :text }
-  end
-
+  
   it 'should parse sexps in list' do
     Client::Parser.run('(d 1) (e 2)').should == { :d => 1, :e => 2 }
   end
@@ -49,5 +53,10 @@ describe Client::Parser do
   it 'should specially parse same sexps with different n-node' do
     Client::Parser.run('(a (n N1) (b value1)) (a (n N2) (b value2))').
       should == { :a => { :N1 => { :b => :value1 }, :N2 => { :b => :value2 }}}
+  end
+
+  it 'should parse complex sexps' do
+    Client::Parser.run('(time (now 696.94))(GS (t 0.00) (pm BeforeKickOff))(GYR (n torso) (rt -0.24 3.30 -0.06))').
+      should == {:time=>{:now=>696.94}, :GS=>{:t=>0.0, :pm=>:BeforeKickOff}, :GYR=>{:torso=>{:rt=>[-0.24, 3.3, -0.06]}}}
   end
 end
