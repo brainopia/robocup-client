18b1069ec7ae0b335b00dec18367d53f3ca254aa
brainopia <ravwar@gmail.com>
2009-08-15 15:18:11 +0400

    Refactor out a new method Client.communicate

--- lib/client.rb
+++ lib/client.rb
@@ -11,26 +11,24 @@ module Client
   Server = ARGV.first || '127.0.0.1' unless Client.const_defined? 'Server'
 
   def connect
-    @thread = Thread.new do
-      Socket.open Server do |socket|
-        commands = Robot.commands
-        loop do
-          unless commands.empty?
-            socket.puts commands.slice!(0, commands.size).join
-          end
-          Robot.data = Mapper.new(Parser.run socket.gets)
-        end
-      end
-    end
+    @thread = Thread.new { Socket.open(Server) {|it| communicate it }}
     sleep 0.1 # give time for a socket to be properly initialized in the second thread
   end
-  
+
   def disconnect
     @thread && @thread.exit
   end
-  
+
   def reconnect
     disconnect
     connect
+  end
+
+  def communicate(socket)
+    commands = Robot.commands
+    loop do
+      socket.puts commands.slice!(0, commands.size).join unless commands.empty?
+      Robot.data = Mapper.new(Parser.run socket.gets)
+    end
   end
 end
