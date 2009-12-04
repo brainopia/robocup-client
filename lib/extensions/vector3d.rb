5ad11077bbfb7abf2380dbf97a882255f2e04e59
brainopia <ravwar@gmail.com>
2009-12-04 10:36:16 +0300

    Add value, zero, scalar_projection, angle_between methods for Vector3d

--- lib/extensions/vector3d.rb
+++ lib/extensions/vector3d.rb
@@ -1,13 +1,16 @@
-# TODO: Vector3d < Struct(:x,:y,:z)
 class Vector3d
   attr_reader :x,:y,:z
+
+  def self.zero
+    self[0,0,0]
+  end
 
   def self.[](x,y,z)
     new x,y,z
   end
 
   def initialize(x,y,z)
-    @x,@y,@z = x.to_f,y.to_f,z.to_f
+    @x,@y,@z = x,y,z
   end
 
   def -@
@@ -34,8 +37,12 @@ class Vector3d
     x*vector.x + y*vector.y + z*vector.z
   end
 
+  def scalar_projection(vector)
+    dot_product(vector) / vector.norm
+  end
+
   def projection(vector)
-    vector.unit * dot_product(vector) / vector.norm
+    vector.unit * scalar_projection(vector)
   end
 
   def cross_product(vector)
@@ -54,8 +61,16 @@ class Vector3d
     norm == 1
   end
 
+  def zero?
+    self == Vector3d.zero
+  end
+
   def norm
     Math.sqrt x**2 + y**2 + z**2
+  end
+
+  def value
+    Vector3d[*[x,y,z].map {|it| it.value.default_round }]
   end
 
   def to_a
@@ -64,5 +79,14 @@ class Vector3d
 
   def to_s
     to_a.inspect
+  end
+
+  def angle_between(v1, v2)
+    plane_direction = v1.cross_product v2
+    y_axis = plane_direction.cross_product v1
+    x = v2.scalar_projection v1
+    y = y_axis.zero? ? 0 : v2.scalar_projection(y_axis)
+    sign = (dot_product(plane_direction) >= 0) ? 1 : -1
+    sign * Math.atan2(y, x)
   end
 end
