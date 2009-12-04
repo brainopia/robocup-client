9bf2e2df68c974888c2d254ea46f7581691342b0
brainopia <ravwar@gmail.com>
2009-12-04 10:34:56 +0300

    Add Numeric#default_round

--- lib/extensions/numeric.rb
+++ lib/extensions/numeric.rb
@@ -1,5 +1,6 @@
 class Numeric
   DEGREES_IN_RADIANS = 180 / Math::PI
+  ROUND_DEGREE = 4
 
   def degrees
     self / DEGREES_IN_RADIANS
@@ -11,5 +12,9 @@ class Numeric
 
   def round_to(degree)
     (self * 10**degree).round.to_f / 10**degree
+  end
+
+  def default_round
+    ROUND_DEGREE ? round_to(ROUND_DEGREE) : self
   end
 end
